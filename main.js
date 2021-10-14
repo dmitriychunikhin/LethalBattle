d20e565f4f19ff8d76a4a1021cb8df81d996bf66
Дмитрий Чунихин <chunihin-df@yandex.ru>
2021-10-14 13:59:34 +0500

    homework-2 querySelector was moved outside createPlayer func

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+const arenas = document.querySelector(".arenas");
+
 const player1 = {
     name: "Scorpion",
     hp: 50,
@@ -22,6 +24,7 @@ const player2 = {
 createPlayer("player1", player1);
 createPlayer("player2", player2);
 
+
 function createPlayer(className, player) {
     const root = document.createElement("div");
     root.classList.add(className);
@@ -31,7 +34,7 @@ function createPlayer(className, player) {
 
     const life = pbar.appendChild(document.createElement("div"));
     life.classList.add("life");
-    life.innerText = player.hp;
+    life.style.width = `${player.hp}%`;
 
     const name = pbar.appendChild(document.createElement("div"));
     name.classList.add("name");
@@ -43,6 +46,5 @@ function createPlayer(className, player) {
     const charImg = character.appendChild(document.createElement("img"));
     charImg.src = player.img;
 
-    const arenas = document.querySelector(".arenas");
     arenas.appendChild(root);
 }
