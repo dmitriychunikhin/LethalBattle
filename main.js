a743507cf5991fda207f7030c9ca0fae4bfb0569
Дмитрий Чунихин <chunihin-df@yandex.ru>
2021-10-16 23:34:48 +0500

    homework-3

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 
+const $arenas = document.querySelector(".arenas");
+const $rndBtn = document.querySelector("button.button");
+let winner = null;
 
-const scorpion = {
+const player1 = {
+    player: 1,
     name: "Scorpion",
-    hp: 50,
+    hp: 100,
     img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
     weapon: ["hook"],
     attack: function () {
@@ -10,9 +14,10 @@ const scorpion = {
     }
 }
 
-const subzero = {
+const player2 = {
+    player: 2,
     name: "Sub-Zero",
-    hp: 70,
+    hp: 100,
     img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
     weapon: ["ice"],
     attack: function () {
@@ -20,32 +25,59 @@ const subzero = {
     }
 }
 
-const arenas = document.querySelector(".arenas");
-arenas.appendChild(createPlayer("player1", scorpion));
-arenas.appendChild(createPlayer("player2", subzero));
+$arenas.appendChild(createPlayer(player1));
+$arenas.appendChild(createPlayer(player2));
+
+$rndBtn.addEventListener("click", function () {
+    !winner && changeHP(player1);
+    !winner && changeHP(player2);
+});
 
 
-function createPlayer(className, player) {
-    const root = document.createElement("div");
-    root.classList.add(className);
+function createElement(tagName, classNames) {
+    const el = document.createElement(tagName);
+    if (classNames) {
+        el.classList.add(classNames);
+    }
+    return el;
+}
 
-    const pbar = root.appendChild(document.createElement("div"));
-    pbar.classList.add("progressbar");
+function createPlayer(player) {
+    const root = createElement("div", `player${player.player}`);
 
-    const life = pbar.appendChild(document.createElement("div"));
-    life.classList.add("life");
-    life.style.width = `${player.hp}%`;
+    const pbar = root.appendChild(createElement("div", "progressbar"));
 
-    const name = pbar.appendChild(document.createElement("div"));
-    name.classList.add("name");
+    const life = pbar.appendChild(createElement("div", "life"));
+    life.style.width = player.hp > 0 ? `${player.hp}%` : '0';
+
+    const name = pbar.appendChild(createElement("div", "name"));
     name.innerText = player.name;
 
-    const character = root.appendChild(document.createElement("div"));
-    character.classList.add("character");
+    const character = root.appendChild(createElement("div", "character"));
 
-    const charImg = character.appendChild(document.createElement("img"));
+    const charImg = character.appendChild(createElement("img"));
     charImg.src = player.img;
 
     return root;
+}
+
+function changeHP(player) {
+    player.hp -= Math.ceil((Math.random() || 0.05) * 20);
+    const $life = document.querySelector(`.player${player.player} .life`);
+    $life.style.width = player.hp > 0 ? `${player.hp}%` : '0';
+
+    if (player.hp <= 0) {
+        setWinner(player.player === 1 ? player2 : player1);
+    }
+}
+
+
+function setWinner(player) {
+    winner = player;
+    $rndBtn.disabled = true;
+
+    const $title = $arenas.appendChild(createElement("div", "loseTitle"));
+    $title.innerText = `${player.name} wins`;
+    $arenas.appendChild($title);
 
 }
