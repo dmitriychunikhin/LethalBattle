c95d2565b731c1ff9c3423d84626fb9ddb79e345
Дмитрий Чунихин <chunihin-df@yandex.ru>
2021-10-24 02:57:13 +0500

    homework-6

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+document.body.appendChild(document.createElement("script")).src = "logTemplates.js";
 
 const $arenas = document.querySelector(".arenas");
 const $rndBtn = document.querySelector("button.button");
 const [$reloadBtnRoot, $reloadBtn] = createReloadButton();
 const $playerAttackForm = document.querySelector(".arenas .control");
-let winner = null;
+const $chat = document.querySelector(".chat");
 
 const HIT = {
     head: 30,
@@ -81,7 +82,7 @@ function attackUI(form) {
         item.checked = false;
     }
 
-    return res; 
+    return res;
 }
 
 function attackAI() {
@@ -94,31 +95,109 @@ function attackAI() {
     }
 }
 
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
+function generateLogs(type, { player1, player2, hit }) {
+    const lines = logs[type];
+    const tmpl = Array.isArray(lines) ? lines[randomInt(0, lines.length - 1)] : `${lines}`;
+    const now = new Date();
+    const nowTime = now.toTimeString().substr(0, 5);
 
-$reloadBtn.addEventListener("click", function () {
-    window.location.reload();
-});
+    const msgBuilder = {
+        text: '',
+        add: function (text) {
+            return { ...this, text: `${this.text}${text}` };
+        },
 
-$playerAttackForm.addEventListener("submit", function (e) {
-    e.preventDefault();
+        replace: function (tmpl, params) {
+            let text = tmpl;
+            for (let [key, val] of Object.entries(params ?? {})) {
+                text = text.replace(`[${key}]`, val);
+            }
+            return this.add(text);
+        },
 
-    const p1Resolve = player1.attack(e.target);
-    const p2Resolve = player2.attack(e.target);
+        addTime: function () { return this.add(`${nowTime} - `) },
+        addHit: function (hit) { return this.add(` -${hit}`) },
+        addHP: function (hp) { return this.add(` [${hp}/100]`) },
 
-    if (p1Resolve.attack !== p2Resolve.defence) {
-        player2.acceptAttack(p1Resolve.hit);
-    }
+        build: function (type) {
+            switch (type) {
+                case 'start':
+                    return this.replace(tmpl, { time: nowTime, player1: player1.name, player2: player2.name });
 
-    if (p2Resolve.attack !== p1Resolve.defence) {
-        player1.acceptAttack(p2Resolve.hit);
-    }
+                case 'hit':
+                    return this
+                        .addTime()
+                        .replace(tmpl, {
+                            playerKick: player1.name,
+                            playerDefence: player2.name
+                        })
+                        .addHit(hit)
+                        .addHP(player2.hp);
 
-    trySetWinner();
+                case 'defence':
+                    return this
+                        .addTime()
+                        .replace(tmpl, {
+                            playerDefence: player2.name,
+                            playerKick: player1.name
+                        });
 
-});
+                case 'end':
+                    return this.replace(tmpl, { playerWins: player1.name, playerLose: player2.name });
 
+                case 'draw':
+                    return this.replace(tmpl);
+
+                default:
+                    return null;
+            }
+        }
+    };
+
+    const msg = msgBuilder.build(type);
+
+    if (msg) $chat.insertAdjacentHTML("afterbegin", `<p>${msg.text}</p>`);
+}
+
+
+window.onload = function () {
+
+    generateLogs('start', { player1, player2 });
+
+    $arenas.appendChild(createPlayer(player1));
+    $arenas.appendChild(createPlayer(player2));
+
+    $reloadBtn.addEventListener("click", function () {
+        window.location.reload();
+    });
+
+
+    $playerAttackForm.style.visibility = 'visible';
+
+    $playerAttackForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const p1Resolve = player1.attack(e.target);
+        const p2Resolve = player2.attack(e.target);
+
+        if (p1Resolve.attack !== p2Resolve.defence) {
+            player2.acceptAttack(p1Resolve.hit);
+            generateLogs('hit', { player1, player2, hit: p1Resolve.hit });
+        } else {
+            generateLogs('defence', { player1, player2 });
+        }
+
+        if (p2Resolve.attack !== p1Resolve.defence) {
+            player1.acceptAttack(p2Resolve.hit);
+            generateLogs('hit', { player2: player1, player1: player2, hit: p2Resolve.hit });
+        } else {
+            generateLogs('defence', { player2: player1, player1: player2 });
+        }
+
+        trySetWinner();
+
+    });
+}
 
 
 function createElement(tagName, classNames) {
@@ -168,7 +247,7 @@ function createPlayer(player) {
 function trySetWinner() {
     if (player1.hp > 0 && player2.hp > 0) return;
 
-    winner = player1.hp === player2.hp ? null : player1.hp > player2.hp ? player1 : player2;
+    const [winner, loser] = player1.hp === player2.hp ? [null, null] : player1.hp > player2.hp ? [player1, player2] : [player2, player1];
 
     $rndBtn.disabled = true;
 
@@ -177,5 +256,7 @@ function trySetWinner() {
     $arenas.appendChild($title);
 
     document.querySelector(".arenas").appendChild($reloadBtnRoot);
+
+    generateLogs(winner ? 'end' : 'draw', { player1: winner, player2: loser });
 
 }
