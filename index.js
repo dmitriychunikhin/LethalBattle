730f64debfbc06788c128f7641b98d479ba454ac
Дмитрий Чунихин <chunihin-df@yandex.ru>
2021-10-30 07:54:27 +0500

    homework-9.4 replace API url protocol with https:

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const $player2 = document.querySelector('.player2');
 function createEmptyPlayerBlock() {
     const el = createElement('div', ['character', 'div11', 'disabled']);
     const img = createElement('img');
-    img.src = 'http://reactmarathon-api.herokuapp.com/assets/mk/avatar/11.png';
+    img.src = 'https://reactmarathon-api.herokuapp.com/assets/mk/avatar/11.png';
     el.appendChild(img);
     $parent.appendChild(el);
 }
